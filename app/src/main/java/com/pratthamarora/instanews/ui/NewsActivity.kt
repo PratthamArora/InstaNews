1c8d8a6d9b17b70629d4e7115857660d4204b4b7
PratthamArora <arora.prattham@gmail.com>
2020-05-02 18:40:33 +0530

    added bottom navigation with nav controller

--- app/src/main/java/com/pratthamarora/instanews/ui/NewsActivity.kt
+++ app/src/main/java/com/pratthamarora/instanews/ui/NewsActivity.kt
@@ -1,13 +1,18 @@
 package com.pratthamarora.instanews.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.pratthamarora.instanews.R
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
