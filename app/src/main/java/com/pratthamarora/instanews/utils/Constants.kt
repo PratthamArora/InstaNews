909823076ebcf0333578f8d6ca88fe085de2e4e2
Prattham Arora <55047418+PratthamArora@users.noreply.github.com>
2020-05-25 17:26:00 +0530

    update

--- app/src/main/java/com/pratthamarora/instanews/utils/Constants.kt
+++ app/src/main/java/com/pratthamarora/instanews/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.pratthamarora.instanews.utils
 
 class Constants {
     companion object {
-        const val API_KEY = "8045a8b7c1cb4e509208ed581bb690e3"
+        const val API_KEY = "Your_Api_Key"
         const val BASE_URL = "https://newsapi.org"
         const val QUERY_PAGE_SIZE = 20
 
